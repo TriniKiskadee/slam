3acd71a99316ca5cc81e29b7b9bfe0f407003522
TriniKiskadee <renaudsennon@gmail.com>
2024-05-16 00:28:21 -0400

    updated triangulate

--- slam.py
+++ slam.py
@@ -27,12 +27,23 @@ K_inv = np.linalg.inv(K)
 
 # Main classes
 mapp = Map()
-display = Display(W, H) if os.getenv("D2D") is not None else None
+display = Display(W, H) if os.getenv("D2D") is 1 else None
 
 
 def triangulate(pose1, pose2, pts1, pts2):
-    return cv2.triangulatePoints(pose1[:3], pose2[:3], pts1.T, pts2.T).T
+    ret = np.zeros((pts1.shape[0], 4))
+    pose1 = np.linalg.inv(pose1)
+    pose2 = np.linalg.inv(pose2)
+    for i, p in enumerate(zip(pts1, pts2)):
+        A = np.zeros((4, 4))
+        A[0] = p[0][0] * pose1[2] - pose1[0]
+        A[1] = p[0][1] * pose1[2] - pose1[1]
+        A[2] = p[1][0] * pose2[2] - pose2[0]
+        A[3] = p[1][1] * pose2[2] - pose2[1]
+        _, _, vt = np.linalg.svd(A)
+        ret[i] = vt[3]
 
+    return ret
 
 def process_frame(image):
     image = cv2.resize(image, (W, H))
